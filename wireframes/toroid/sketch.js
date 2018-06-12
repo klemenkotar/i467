c9de37b95895266edbb12476367ef853d32f9198
Klemen Kotar <klemenkotar@Klemens-MacBook-Pro.local>
2018-06-12 00:00:34 -0600

    added support for different screen sizes

--- wireframes/toroid/sketch.js
+++ wireframes/toroid/sketch.js
@@ -12,7 +12,11 @@ function draw(){
   rotateZ(frameCount * 0.01);
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
-  torus(120, 40);
+  if (int(window.innerWidth) < 500) {
+     torus(window.innerWidth/7, 20);
+  } else {
+    torus(120, 40);
+  }
   pop();
   
 }
